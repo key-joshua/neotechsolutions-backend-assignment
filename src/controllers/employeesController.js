73e38b28c1b0a9095042ce2bcccb84aab740d8e4
yourfhighness <k.joshua855@gmail.com>
2021-12-02 21:56:09 +0200

    Ft Completed Delete Employee Endpoint

--- src/controllers/employeesController.js
+++ src/controllers/employeesController.js
@@ -1,4 +1,4 @@
-import { INTERNAL_SERVER_ERROR, CREATED } from 'http-status';
+import { INTERNAL_SERVER_ERROR, CONFLICT, CREATED, OK, NOT_FOUND } from 'http-status';
 import employeesHelper from '../helpers/employeesHelper';
 import responseHelper from '../services/responseHelper';
 
@@ -10,16 +10,46 @@ import responseHelper from '../services/responseHelper';
 */
 class EmployeesController {
   /**
-     * Handle createEmployees.
+     * Handle createEmployee.
      * @param {object} req user request.
      * @param {object} res data response.
      * @returns {object} returned object of data.
      */
   static async createEmployee(req, res) {
     try {
-      const data = await employeesHelper.saveData(req.body);
+      let data = await employeesHelper.employeeExist('email', req.body.email);
+      if (data) {
+        responseHelper.handleError(CONFLICT, 'Email already exist.');
+        return responseHelper.response(res);
+      }
 
+      data = await employeesHelper.saveData(req.body);
       responseHelper.handleSuccess(CREATED, 'Employee created successfully.', data);
+      return responseHelper.response(res);
+    } catch (error) {
+      return res.status(INTERNAL_SERVER_ERROR).json({
+        status: INTERNAL_SERVER_ERROR,
+        message: error.toString()
+      });
+    }
+  }
+
+  /**
+     * Handle deleteEmployee.
+     * @param {object} req user request.
+     * @param {object} res data response.
+     * @returns {object} returned object of data.
+     */
+  static async deleteEmployee(req, res) {
+    try {
+      const data = await employeesHelper.employeeExist('_id', req.params.employeeId);
+      if (!data || data.banned === true) {
+        responseHelper.handleError(NOT_FOUND, `Employee with id ${req.params.employeeId} not found.`);
+        return responseHelper.response(res);
+      }
+
+      await employeesHelper.banEmployee(req.params.employeeId);
+      responseHelper.handleSuccess(OK, 'Employee deleted successfully.');
       return responseHelper.response(res);
     } catch (error) {
       return res.status(INTERNAL_SERVER_ERROR).json({
--- src/routes/employeesRoute.js
+++ src/routes/employeesRoute.js
@@ -5,6 +5,7 @@ import { validateCreateEmployee } from '../middlewares/schemaMiddleware';
 const employeesRouter = Router();
 
 employeesRouter
+  .delete('/delete-employee/:employeeId', employees.deleteEmployee)
   .post('/create-employee', validateCreateEmployee, employees.createEmployee);
 
 export default employeesRouter;
